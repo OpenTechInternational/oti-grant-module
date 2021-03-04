ac961832d79ff5b16132a7258ba662e8dab96666
krtonga <kristen.tonga@gmail.com>
2021-03-04 18:08:19 +0000

    Move email logic into email_service.py and hook up Airtable to configure email pushes

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Development
-## API keys
+## Integrations
+This project involves the following third parties, the accounts for which will need to be set up.
+
+- Airtable 
+- Give Butter
+
+## Adding API keys
 1. **Create a `.env` file in the base directory.** This file can be used for any variables that you need to keep secret, including API keys. This file should not be checked into git. This exclusion will happen automatically, as `.env` is listed in the `.gitignore` file.
 3. **Add the following variables to the `.env` file.**
 
@@ -8,4 +14,5 @@
     AIRTABLE_API_KEY=[your_key]
     ```
 
-What needs to be installed to run this project?
+## Running Locally
+TBD
--- email_service.py
+++ email_service.py
@@ -1,13 +1,91 @@
 """For managing email related services."""
 
+import os
+import traceback
+import airtable
+from apscheduler.schedulers.background import BackgroundScheduler
 
-def build_confirmation_msg(name: str):
+
+AIRTABLE_BASE = os.getenv('AIRTABLE_BASE')
+AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
+
+minute_jobs = []
+
+class BaseEmailConfig:
+  MAIL_SERVER = os.getenv('MAIL_SERVER', 'smpt.gmail.com')
+  # MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.mailgun.org')
+  MAIL_PORT = os.getenv('MAIL_SERVER_PORT', 2525)
+  MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+  MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', True)
+  MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+
+def start_email_service():
+  """Get emails specifications based on Airtable EMAILS Table"""
+  airtable_emails = airtable.Airtable(AIRTABLE_BASE, 'emails', AIRTABLE_API_KEY)
+  email_specs = airtable_emails.get_all(view='Grid view')
+  # TODO: add message specifically when error is because of templeting error
+  # print(email_specs)
+
+  for record in email_specs:
+    if int(record['fields']['Frequency in Minutes']) == 1:
+      minute_jobs.append(record)
+
+  sched = BackgroundScheduler(daemon=True)
+  sched.add_job(each_minute,'interval',seconds=10)
+  sched.start()
+
+def each_minute():
+  print("Minute Scheduler is alive!")
+  for record in minute_jobs:
+    table_name = record['fields']['Table Name']
+    message = record['fields']['Message']
+    variables = record['fields']['Variables'].split(',')
+      
+    send_confirmation_emails(table_name, message, variables)
+
+def send_confirmation_emails(table_name: str, message_template: str, variables_arr: []):
+  """Send confirmation emails to all the To Process folks.
+
+  Should look something like:
+      1. get all records which haven't gotten emails or errors
+      2. iterate through the records
+      2a. send the emails
+      2a1. try: sending the email
+      2a2. except: error sending the email
+      2a3. mark the error that was found
+      2b. mark the records as processed
+    
+  """
+  table = airtable.Airtable(AIRTABLE_BASE, table_name, AIRTABLE_API_KEY)
+  to_process = table.get_all(view='To process')
+  # print(to_process)
+
+  for record in to_process:
+    try:
+      print(record)
+      email = record['fields']['Email']
+      msg = build_confirmation_msg(message_template, variables_arr, record)
+      print(msg)
+      send_email(email, msg)
+        # verify this works to mark the record as processed:
+      fields = {'Processed': True}
+      table.update(record['id'], fields)
+    except Exception as e:
+      print(f"Error sending email: {traceback.print_tb(e.__traceback__)}")
+      fields = {'Error': str(traceback.format_exc())}
+      table.update(record['id'], fields)
+
+def build_confirmation_msg(message_template: str, variables_arr: [], record):
   """Returns the full confirmation email as a string
-  
   Note: some email services also support sending HTML for future purposes.
   """
+  inserts = []
+  for variable in variables_arr:
+    inserts.append(record['fields'][variable.strip()])
+  message = message_template.format(*inserts)
+  # print(message)
 
-  return f"This is a default confirmation email for {name}"
+  return message
 
 def send_email(to: str, msg: str):
   """Send an email with the provided msg to the given address from: TODO:(default `from` email here)
